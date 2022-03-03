52e72dffe1231cebd9839a6cdb6c59b9fa2eccc2
Colleen Murray <colleenkhm@gmail.com>
2022-03-03 17:24:32 -0600

    rough outline of functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,30 @@
 // initial event that loads first question/starts countdown
+var startQuiz = function() {
 
-// event listener for selecting answer
+}
+// questions/event listener for selecting answer
+var questions = [
+    prompt: "Arrays in JavaScript can be used to store ____." 
 
-// verify if answer is correct or incorrect
+    prompt: 
+]
+var selectAnswer = function() {
 
-// if incorrect, deduct time from countdown
+}
+// verify if answer is correct or incorrect (must be inside startQuiz function)
+if (playerAnswer === rightAnswer){
+    alert('Wrong!')
+    //function for subtracting 10 seconds from time goes here (timeRemaining-10)
+} else //continue loop/function for loading new question
 
-// refresh browser with new question/multiple choice options
+//when time runs out, endQuiz (if timeRemaining=0... or use while loop for countdown)
+var endQuiz = function() {
 
+}
 // calculate a final score based on time completed (high score=high time remaining)
-
+var finalScore = timeRemaining
 // store high scores/player initials
 
 // option to replay
 
+startQuiz();
