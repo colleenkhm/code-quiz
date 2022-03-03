1db07cb07d59ab41ef5a0693dedd2338692afc1c
Colleen Murray <colleenkhm@gmail.com>
2022-03-02 20:41:03 -0600

    add pseudo code to js file

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+// initial event that loads first question/starts countdown
+
+// event listener for selecting answer
+
+// verify if answer is correct or incorrect
+
+// if incorrect, deduct time from countdown
+
+// refresh browser with new question/multiple choice options
+
+// calculate a final score based on time completed (high score=high time remaining)
+
+// store high scores/player initials
+
+// option to replay
+
