3e784a54675d22fff0ef58dab9855d2ef31e9e6e
Colleen Murray <colleenkhm@gmail.com>
2022-07-05 09:15:26 -0500

    populated question and answer fields with array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,104 @@
 // VARIABLES
-//__elements to link to html__
-    // scoreboard
+// __elements to link to html__
+//     scoreboard
+    const highScores = document.getElementById("highScores");
     // const scoreboard = document.getElementById("#scoreboard");
         // scoreboard slot    
         // return to home
     // timer
-    const timerDisplay = document.getElementById("#countdown")
+    const timerDisplay = document.getElementById("countdown")
     // quiz rules
-    const quizRules = document.getElementById("#quizRules");
+    // const quizRules = document.getElementById("#quizRules");
     // question
     // answer1
+    const answerOne = document.getElementById("0");
     // answer2
+    const answerTwo = document.getElementById("1");
     // answer3
+    const answerThree = document.getElementById("2");
     // answer4
+    const answerFour = document.getElementById("3");
     // play again
     // correct alert
-    const correctAlert = document.getElementById("#correct-alert");
+    const correctAlert = document.getElementById("correctAlert");
     // incorrect alert
-    const incorrectAlert = document.getElementById("#incorrect-alert");
+    const incorrectAlert = document.getElementById("incorrectAlert");
+    // array of questions
+const questionsList = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        answer1: "Strings",
+        answer2: "Booleans",
+        answer3: "Alerts",
+        answer4: "Numbers",
+        right: "answer3"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ____.",
+        answer1: "Quotes",
+        answer2: "Curly Brackets",
+        answer3: "Parenthesis",
+        answer4: "Square Brackets",
+        right: "answer3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        answer1: "Numbers and Strings",
+        answer2: "Other Arrays",
+        answer3: "Booleans",
+        answer4: "All of the above",
+        right: "answer4"
+    },
+    {
+        question: "String values must be enclosed within ___ when being assigned to variables.",
+        answer1: "Commas",
+        answer2: "Curly Brackets",
+        answer3: "Quotes",
+        answer4: "Parenthesis",
+        right: "answer3"
+    },
+    {
+        question: "A very useful tool during development and debugging for printing ",
+        answer1: "JavaScript",
+        answer2: "Terminal/Bash",
+        answer3: "For Loops",
+        answer4: "console.log()",
+        right: "answer4"
+    },
+]
 
+function startTimer() {
+    setInterval(updateTime, 1000);
+//     // const questionsDisplay = document.getElementById("questionsDisplay");
+//     questionsDisplay.classList.remove("hidden")
+// //     updateTime();
+//     var q = questionsList[currentQuestion];
+//     question.innerHTML = "<p>" + q + "</p>"
+}
+
+function populateQuiz() {
+    for (var i = 0; i < 5; i++) {
+        questionShown.innerHTML = "<p>" + questionsList[i].question + "</p>"
+        choice1.innerHTML = "<p>" + questionsList[i].answer1 + "</p>"
+        choice2.innerHTML = "<p>" + questionsList[i].answer2 + "</p>"
+        choice3.innerHTML = "<p>" + questionsList[i].answer3 + "</p>"
+        choice4.innerHTML = "<p>" + questionsList[i].answer4 + "</p>"
+    }
+}
+
+function startQuiz() {
+    quizRules.classList.add("hidden")
+    questionsDisplay.classList.remove("hidden")
+    console.log("quiz has started");
+    startTimer()
+    populateQuiz()
+}
+
+// function showHighScores() {
+//     console.log("it works");
+// }
 // FUNCTIONS
 // start quiz
-function startQuiz() {
-    console.log("quiz has started")
-    revealQuestion()
-}
     // starts timer
     // updateTime()
         // when timer reaches 0, displays high scores
@@ -38,18 +112,13 @@ function startQuiz() {
                 // subtracts from time
                 // removes hidden class from "incorrect" footer
     // when user clicks scoreboard, reveals scoreboard
-    function showHighScores() {
-        scoreboard.classList.remove("hidden")
-    }
-        // when user clicks return to home, return to home
+function showHighScores() {
+    const scoreboard = document.getElementById("scoreboard")
+    scoreboard.classList.remove("hidden")
+}
 
-const answerOne = document.getElementById("#0");
-const answerTwo = document.getElementById("#1");
-const answerThree = document.getElementById("#2");
-const answerFour = document.getElementById("#3");
-const rules = document.getElementById("#quizRules");
-const questionsDisplay = document.getElementById("#questions");
-var questionDisplay = document.getElementById("#")
+highScores.addEventListener('click', showHighScores());
+        // when user clicks return to home, return to home
 
 // questions/scoring
 var score = ""; // = time remaining?
@@ -57,51 +126,6 @@ var question = "";
 var answerKey = [];
 var righAnswer = "";
 var initials = "";
-
-
-// array of questions
-const questionsList = [
-    {
-        question: "Commonly used data types DO NOT include:",
-        btnChoice1: "Strings",
-        btnChoice2: "Booleans",
-        btnChoice3: "Alerts",
-        btnChoice4: "Numbers",
-        right: "btnChoice3"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with ____.",
-        btnChoice1 : "Quotes",
-        btnChoice2 : "Curly Brackets",
-        btnChoice3 : "Parenthesis",
-        btnChoice4 : "Square Brackets",
-        right : "btnChoice3"
-    },
-    {
-        question: "Arrays in JavaScript can be used to store:",
-        btnChoice1 : "Numbers and Strings",
-        btnChoice2 : "Other Arrays",
-        btnChoice3 : "Booleans",
-        btnChoice4 : "All of the above",
-        right : "btnChoice4"
-    },
-    {
-        question: "String values must be enclosed within ___ when being assigned to variables.",
-        btnChoice1 : "Commas",
-        btnChoice2 : "Curly Brackets",
-        btnChoice3 : "Quotes",
-        btnChoice4 : "Parenthesis",
-        right : "btnChoice3"
-    },
-    {
-        question: "A very useful tool during development and debugging for printing ",
-        btnChoice1 : "JavaScript",
-        btnChoice2 : "Terminal/Bash",
-        btnChoice3 : "For Loops",
-        btnChoice4 : "console.log()",
-        right : "btnChoice4"
-    },
-]
 
 // countdown timer
 var intervalId;
@@ -118,67 +142,3 @@ function updateTime() {
     time--;
 }
 
-setInterval(updateTime, 1000);
-
-
-// set original time
-// link OG time variable to dynamic time variable
-// create increment for time change
-// 
-
-
-// function to show selected element/hide others
-function revealQuiz() {
-    questions.classList.remove("hidden")
-    updateTime();
-    var q = questionsList[currentQuestion];
-    question.innerHTML = "<p>" + q + "</p>"
-};
-// function to display quiz/hide rules
-// function showQuiz() {
-//     revealElement()
-// };
-
-
-function startQuiz() {
-    console.log("quiz has started")
-    quizRules.classList.add("hidden")
-    questions.classList.remove("hidden")
-    // displayTime();
-    revealQuiz();
-    
-    // for(var i=0; i < questions.length; i++) {
-    //     var response = '';
-    //     if (response === questions[i]) {
-            
-    //     }
-    //         question++;
-    //     } else {
-
-    //     }
-    // }
-
-}
-
-function showHighScores() {
-    console.log("it works");
-}
-
-// event listener to start quiz
-// btnStart.addEventListener('click', startQuiz());
-highScores.addEventListener('click', showHighScores());
-
-
-// what's needed:
-// Intro page: contains start button to execute StartQuiz()
-// variable(s): start button
-// 1. startQuiz() function that displays first question when start button is clicked
-
-// 2. if statement that alerts "correct!" if answer matches correct answer and then reloads page with new question
-// 3. else statement that alerts "incorrect" if answer does not match correct answer, subtracts 5 seconds from timeRemaining, and reloads page with new question
-// 4. countdown() function to count down time that does not reload when new question is revealed
-// 5. endQuiz() function that displays time remaining and gives option to startQuiz() again
-// 6. addHighScore() function that prompts user to input initials and stores timeRemaining as score in viewHighScores object
-// 7. function that sorts variables in viewHighScores object in terms of highest-lowest
-// 8. event.preventDefault() in startQuiz() function to ensure that page does not reload when new question is revealed
-// 9. append existing outside of startQuiz() function so that high score list continues to add with each playthrough
